afba3b871c75579f8bc43b51c2ffc4694ddb6bdb
M-Azariel <walter.azariel.moreno@gmail.com>
2025-04-03 20:42:37 -0300

    controllers aded

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+import taskRoutes from "./routes/task.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
+
+//Middleweares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
+//Routes
 app.get("/", (req, res)=> res.json({message: "wellcome to my API"}));
+app.use("/api",taskRoutes)
+app.use("/api",authRoutes)
 
+//error handler
 app.use((err, req, res, next)=>{
     res.status(500).json({
         status: "error",
---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,9 @@
+export const getAllTasks = (req, res)=> res.send("obteniendo tareas")
+
+export const getTask = (req, res)=> res.send("obteniendo tare unica")
+
+export const createTask=  (req, res)=> res.send("creando tares")
+
+export const updateTask=  (req, res)=> res.send("actualizando tarea unica")
+
+export const deleteTask=  (req, res)=> res.send("eliminando tarea")
